1fe499ac7bf0570d537d474f2dbe98e985b4dad5
blancamm <blanquita98s@gmail,com>
2021-04-27 23:21:37 +0200

    le emtemos una segunda bola

--- ball.py
+++ ball.py
@@ -1,10 +1,23 @@
 import pygame
 import sys
+import random
 
 ROJO = (255, 0 ,0)
 AZUL = (0, 0, 255)
 VERDE = (0, 255, 0)
 NEGRO = (0,0,0)
+ANCHO = 800
+ALTURA = 600
+
+def rebotax(x):
+    if x <= 0 or x >= ANCHO:
+        return -1  #hay que poner en ambos un menos, porque pasamos de uno a otro(del anterior valor que se ha dado a la variable)
+    return 1
+
+def rebotay(y):
+    if y <= 0 or y >= ALTURA:
+        return -1  #hay que poner en ambos un menos, porque pasamos de uno a otro(del anterior valor que se ha dado a la variable)   
+    return 1
 
 pygame.init()
 
@@ -12,11 +25,19 @@ pantalla = pygame.display.set_mode((800,600))
 
 gameOver= False
 
+#bola 1
 x = 400 #esto es la mitad (se podria poner con variables)
 y = 300
 
-velocidadx = -7
-velocidady = -7
+vx = -7
+vy = -7
+
+#bola 2(la vamos a hacer de forma aleatoria)
+x2 = random.randint(0, 800)
+y2 = random.randint(0, 600)
+vx2 = random.randint(5,15)
+vy2 = random.randint (5,15)
+
 reloj = pygame.time.Clock()
 
 while not gameOver:
@@ -26,18 +47,24 @@ while not gameOver:
         if evento.type == pygame.QUIT:
             gameOver = True
 
-    x += velocidadx
-    y += velocidady
+    x += vx
+    y += vy
+    x2 += vx2
+    y2 += vy2
 
-    if y <= 0 or y >=600: #porque puede que no llegue al 0 como tal como la velocidad -7
-        velocidady = -velocidady
+    vx *=rebotax(x)
+    vy *=rebotay(y)
+    vx2 *=rebotax(x2)
+    vy2 *=rebotay(y2)
 
 
-    if x <= 0 or x >= 800:
-        velocidadx = -velocidadx  #hay que poner en ambos un menos, porque pasamos de uno a otro(del anterior valor que se ha dado a la variable)
+
+
+    
 
     pantalla.fill (NEGRO)
     pygame.draw.circle(pantalla, ROJO, (x, y), 10)
+    pygame.draw.circle(pantalla, VERDE, (x2, y2), 10)
 
 
     pygame.display.flip()
