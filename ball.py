6d1b97a5336958ec751a85e8c04003427979b4e6
blancamm <blanquita98s@gmail,com>
2021-04-28 00:19:13 +0200

    creamos una clase bola para que este todo mas claro

--- ball.py
+++ ball.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-import random
+import random #podriamos poner fom random import randint y asi no tenemos que poner random.randint y solo randint
 
 ROJO = (255, 0 ,0)
 AZUL = (0, 0, 255)
@@ -16,7 +16,7 @@ def rebotax(x):
 
 def rebotay(y):
     if y <= 0 or y >= ALTURA:
-        return -1  #hay que poner en ambos un menos, porque pasamos de uno a otro(del anterior valor que se ha dado a la variable)   
+        return -1   
     return 1
 
 pygame.init()
@@ -25,13 +25,21 @@ pantalla = pygame.display.set_mode((800,600))
 
 gameOver= False
 
+class Bola():
+    def __init__(self, x, y, vx, vy, color):
+        self.x = x
+        self.y= y
+        self.vx = vx
+        self.vy = vy
+        self.color = color
+
 bolas = []
 for _ in range (10):
-    bola = {'x':random.randint(0, ANCHO),
-    'y': random.randint(0, ALTURA),
-    'vx':random.randint(5,10),
-    'vy':random.randint(5,10),
-    'color': (random.randint(0,255), random.randint(0,255), random.randint(0,255))}
+    bola = Bola(random.randint(0, ANCHO), 
+                random.randint(0, ALTURA),
+                random.randint(5,10), 
+                random.randint(5,10), 
+                (random.randint(0,255), random.randint(0,255), random.randint(0,255)))
     bolas.append(bola)
 
 
@@ -48,16 +56,16 @@ while not gameOver:
     #modificacion de estados
 
     for bola in bolas:
-        bola['x'] += bola['vx']
-        bola['y'] += bola['vy']
+        bola.x += bola.vx
+        bola.y += bola.vy
 
-        bola['vy'] *= rebotay(bola['y'])
-        bola['vx'] *= rebotay(bola['x'])
+        bola.vy *= rebotay(bola.y)
+        bola.vx *= rebotay(bola.x)
 
     #gestion de pantalla
     pantalla.fill (NEGRO)
     for bola in bolas:
-        pygame.draw.circle(pantalla, bola['color'], (bola['x'], bola['y']), 10)
+        pygame.draw.circle(pantalla, bola.color, (bola.x, bola.y), 10)
 
 
     pygame.display.flip()
