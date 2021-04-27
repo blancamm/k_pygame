ef2d0edad7a9e00ffefa81995115726b117b6d87
blancamm <blanquita98s@gmail,com>
2021-04-27 21:02:08 +0200

    ultimas cosas de la funcion hola

--- hello.py
+++ hello.py
@@ -1,21 +1,25 @@
-import pygame as pg
+import pygame 
+import sys
 
-pg.init()
-pantalla = pg.display.set_mode((600,400))
-pg.display.set_caption('hola mundo')
+pygame.init()
+pantalla = pygame.display.set_mode((600,400))
+pygame.display.set_caption('hola mundo')
 
 gameOver = False
 
 while not gameOver:
     #Gestion de eventos
-    for evento in pg.event.get():
-        pass
-   
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT():
+            gameOver= True
     #Gestion del estado(actualizar las funciones)
     print('hola mundo')
 
     #Refrescar la pantalla
     pantalla.fill((0, 255, 0)) 
-    pg.display.flip()#e lo mueve a la memoria de la tarjeta grafica. ESENCIAL
+    pygame.display.flip()#e lo mueve a la memoria de la tarjeta grafica. ESENCIAL
+
+pigame.quit()
+sys.exit() #o se puede meter ests dos lineas en una funcion y esta linea se llama a la funcion
 
 
