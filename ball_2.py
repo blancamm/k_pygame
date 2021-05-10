ed9e42f9b96649e0d255fac33f1d15be8e3c6ff1
blancamm <blanquita98s@gmail,com>
2021-05-10 16:38:56 +0200

    Hemos hecho la raqueta, el marcador y el principio de estados de bolas: estoy viva

--- ball_2.py
+++ ball_2.py
@@ -16,30 +16,47 @@ class Marcador(pg.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = (x, y)) #lo referencio segun donde quiero poner la esquina superior izquierda del rectangulo
 
 
-    def update(self): #necesario en los sprite para que se actualiza lo que te da el grupo
+    def update(self,dt): #necesario en los sprite para que se actualiza lo que te da el grupo
         self.image = self.fuente.render(str(self.text), True, self.color)
 
 class Bola(pg.sprite.Sprite): #va a heredar de la lcase sprite sus funcionalidades y atributos
     def __init__(self, x, y):
         #pg.sprite.Sprite.__init__(self) - ESTA SERIA UNA FORMA
-        super().__init__() #ESTA ES LA OTRA FORMA
-        self.image = pg.image.load('./imagenes/ball1.png').convert_alpha() #creo que es importante el nombre del atributo para que luego lo llame sprite
-        self.rect = self.image.get_rect(center = (x,y))#aqui ya no me tengo que preocupar de las dimensiones, lo tiene el propio objeto
-        #lo de arriba es una instancia de la clase rectangulo
+        super().__init__()                  #ESTA ES LA OTRA FORMA
+        self.image = pg.image.load('./imagenes/ball1.png').convert_alpha()  #creo que es importante el nombre del atributo para que luego lo llame sprite
+        self.rect = self.image.get_rect(center = (x,y))                     #aqui ya no me tengo que preocupar de las dimensiones, lo tiene el propio objeto
+                                                                            #lo de arriba es una instancia de la clase rectangulo
+
+
+        self.estoyViva = True
 
 
         self.vx = randint(5,10)*choice([-1, 1])
         self.vy = randint(5,10)*choice([-1, 1])
 
-    def update (self):
-        self.rect.x += self.vx
-        self.rect.y += self.vy
+    def prueba_colision(self, grupo):
+        candidatos = pg.sprite.spritecollide(self, grupo, False) #false para no borrar la raqueta aunque la toque
+        if len(candidatos)>0 :
+            self.vy *=-1
 
-        if self.rect.left <= 0 or self.rect.right >= ANCHO:
-            self.vx *= -1
+    def update (self,dt):
+        if self.estoyViva:
+            self.rect.x += self.vx
+            self.rect.y += self.vy
 
-        if self.rect.top <= 0 or self.rect.bottom >= ALTO: #esto son las distintas posiciones del objeto rectangulo
-            self.vy *= -1
+            if self.rect.left <= 0 or self.rect.right >= ANCHO:
+                self.vx *= -1
+
+            if self.rect.top <= 0 or self.rect.bottom >= ALTO: #esto son las distintas posiciones del objeto rectangulo
+                self.vy *= -1
+
+            if self.rect.bottom >= ALTO:
+                self.estoyViva = False
+        else:
+            self.rect.center = (ANCHO//2, ALTO//2)
+            self.vx = randint(5,10)*choice([-1, 1])
+            self.vy = randint(5,10)*choice([-1, 1])
+            self.estoyViva = True
 
 class Raqueta(pg.sprite.Sprite):
     fotos = ['electric00.png', 'electric01.png', 'electric02.png']
@@ -48,9 +65,11 @@ class Raqueta(pg.sprite.Sprite):
         super().__init__()
         self.imagenes = self.cargaImagenes()
         self.imagen_actual = 0 #actual como indice
+        self.milisegundos_para_cambiar = 1000// FPS *5
+        self.milisegundos_acumulados = 0
         self.image = self.imagenes[self.imagen_actual]
         self.rect = self.image.get_rect(centerx = x, bottom = y)
-        self.vx = 7
+        self.vx = 10
 
     def cargaImagenes(self):
         imagenes = []
@@ -58,7 +77,7 @@ class Raqueta(pg.sprite.Sprite):
             imagenes.append(pg.image.load('./imagenes/{}'.format(fichero)))
         return imagenes
 
-    def update(self):
+    def update(self, dt):
         teclas_Pulsadas = pg.key.get_pressed()
         if teclas_Pulsadas[pg.K_LEFT]:
             self.rect.x -= self.vx
@@ -70,18 +89,26 @@ class Raqueta(pg.sprite.Sprite):
         if self.rect.left <= 0:
             self.rect.left = 0
         if self.rect.right >= ANCHO:
-            self.rect.right = 0
+            self.rect.right = ANCHO
 
-        self.imagen_actual += 1
-        if self.imagen_actual >= len(self.fotos):
-            self.imagen_actual = 0
-
+        self.milisegundos_acumulados += dt
+        if self.milisegundos_acumulados >= self.milisegundos_para_cambiar:
+            self.imagen_actual += 1
+            if self.imagen_actual >= len(self.fotos):
+                self.imagen_actual = 0
+            self.milisegundos_acumulados = 0
         self.image = self.imagenes[self.imagen_actual]
 
 class Game():
     def __init__(self):
         self.pantalla = pg.display.set_mode((ANCHO, ALTO)) 
-        self.botes = 0
+        self.vidas = 3
+
+        #SE CREA ESTOS GRUPOS PARA PODER HACER LA COLISION ENTRE ELLOS
+
+        self.grupoJugador = pg.sprite.Group()
+        #self.grupoPelota = pg.sprite.Group() DE MOMENTO NO ES NECESARIO
+        self.grupoLadrillos = pg.sprite.Group()
         
         self.TodoGroup = pg.sprite.Group ()
 
@@ -92,6 +119,7 @@ class Game():
         self.TodoGroup.add(self.bola) 
 
         self.raqueta = Raqueta(x = ANCHO//2, y=ALTO-30)
+        self.grupoJugador.add(self.raqueta)
         self.TodoGroup.add(self.raqueta)
 
 
@@ -101,7 +129,7 @@ class Game():
         reloj= pg.time.Clock()
         contandor_milisegundos = 0
         segundero = 0
-        while not game_Over:
+        while not game_Over and self.vidas > 0:
             dt = reloj.tick(FPS)
             contandor_milisegundos += dt
 
@@ -114,7 +142,13 @@ class Game():
                     game_Over= True
 
             self.cuentaSegundos.text = segundero
-            self.TodoGroup.update()
+            self.bola.prueba_colision(self.grupoJugador)
+
+            self.TodoGroup.update(dt)
+
+            if not self.bola.estoyViva:
+                self.vidas -= 1
+                print(self.vidas)
 
             self.pantalla.fill((0,0,0))
             
